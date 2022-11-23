634bb552653b9c49ffc3d2eccc7440b1f80c633b
Michael Henke <m433270+aphex3k@users.noreply.github.com>
2022-11-22 17:50:13 -0800

    1st attempt at dynamically generating passwords at first run

---
+++ firstrun.sh
@@ -0,0 +1,17 @@
+PASSWORDLENGTH=32
+PASSWORDFILE="lnd/watchtower.password"
+# 0. Check if a password file exists
+if [ ! -f $PASSWORDFILE ] || [ $(wc -c $PASSWORDFILE | sed "s/  */ /g"  | cut -d " " -f 2 || echo 0) -ne $PASSWORDLENGTH ]; then
+
+    # tr needs to operate on binary input from urandom
+    export LC_CTYPE=C
+
+    # 1. generate a password file for the lnd wallet if it doesnt exist already
+    < /dev/urandom tr -dc "[:alnum:]" | head -c$PASSWORDLENGTH > $PASSWORDFILE
+
+    # 2. generate tor control password and set up lnd and tor
+    TORPASSWORD=$(< /dev/urandom tr -dc "[:alnum:]" | head -c$PASSWORDLENGTH)
+    HASHED_TOR_PASSWORD=$(docker run --rm lncm/tor:0.4.7.9 --hash-password $TORPASSWORD)
+    sed -i .bak "s/tor.password=.*/tor.password=$TORPASSWORD/" lnd/lnd.conf && rm lnd/lnd.conf.bak
+    sed -i .bak "s/HashedControlPassword .*/HashedControlPassword $HASHED_TOR_PASSWORD/" tor/torrc-lnd && rm tor/torrc-lnd.bak
+fi 
